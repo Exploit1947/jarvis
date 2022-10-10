1912817a0e8155dc70e1bb1bcecefc8089f289e8
Divyam-Pradhan <86609734+Divyam-Pradhan@users.noreply.github.com>
2022-10-10 22:10:38 +0530

    Added code for inverted half triangle
    Update codelines to display inverted half-triangle  pattern using star

--- test.c
+++ test.c
@@ -16,5 +16,14 @@ int main() {
       }
       printf("\n");
    }
+   
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = rows; i >= 1; --i) {
+      for (j = 1; j <= i; ++j) {
+         printf("* ");
+      }
+      printf("\n");
+   }
    return 0;
 }
